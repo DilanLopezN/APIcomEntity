fd8fb212053be57664cd299a1b0625da8eba4ac7
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-27 12:24:41 -0300

    connect entity to sqlserver

--- Context/ScheduleContext.cs
+++ Context/ScheduleContext.cs
@@ -2,20 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using APIsComCsharp.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace APIsComCsharp.Context
 {
   public class ScheduleContext : DbContext
   {
     public ScheduleContext(DbContextOptions<ScheduleContext> options) : base(options)
-
     {
 
     }
 
     public DbSet<Contact> Contacts { get; set; }
-
-
   }
 }
